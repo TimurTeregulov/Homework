18a866740790c5ac70a2124f8f0d7479f0b97288
Timur <tteregulov65@gmail.com>
2023-05-29 00:08:21 +0400

    Add Menu

--- GitCalc/Program.cs
+++ GitCalc/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            decimal num1;
+            decimal num2;
+            char choice;
+            decimal result;
+            Console.Write("Enter first number: ");
+            num1 = decimal.Parse(Console.ReadLine());
+            Console.Write("Enter action: ");
+            choice = char.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            num2 = decimal.Parse(Console.ReadLine());
+            switch (choice) 
+            {
+                case '+':
+
+                    break;
+
+                case '-':
+
+                    break;
+
+                case '*':
+
+                    break;
+
+                case '/':
+
+                    break;
+
+            }
+
         }
     }
 }
